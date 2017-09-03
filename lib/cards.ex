ce565b25efe7b211fac8363674ff3ad3b770206c
Stefan-Lucian Bucur <slbucur@gmail.com>
2017-09-03 17:39:18 +0300

    Added doc tests.

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,23 +3,22 @@ defmodule Cards do
   Provides methods for creating and handling a deck of cards
   ## Example usage
       iex> deck = Cards.create_deck
-      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> {hand, _deck} = Cards.deal(deck, 1)
       iex> hand
-      ["Ace of spades"]
+      ["Ace of Spades"]
   """
 
   @doc """
-  Creates an array that represents a deck of cards
+    Creates an array that represents a deck of cards
 
-  ## Examples
-  ```
-  Cards.create_deck
-  ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
-  "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
-  "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
-  "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds",
-  "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
-  ```
+    ## Examples
+
+      iex> Cards.create_deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+      "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+      "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+      "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds",
+      "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
   """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
@@ -32,31 +31,37 @@ defmodule Cards do
   end
 
   @doc """
-  Shuffles a deck randomly
+    Shuffles a deck randomly
   """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
   @doc """
-  Checks if a deck contains a card
+    Checks if a deck contains a card
+
+    ## Examples
+        iex> deck = Cards.create_deck
+        iex> Cards.contains?(deck, "Ace of Spades")
+        true
+
   """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
   @doc """
-  Takes `hand_size` cards out of the deck and returns two arrays:
+    Takes `hand_size` cards out of the deck and returns two arrays:
 
-  * one with `hand_size` cards
-  * another one with the remaining deck
+    * one with `hand_size` cards
+    * another one with the remaining deck
 
   ## Examples
 
       iex> deck = Cards.create_deck
-      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> {hand, _deck} = Cards.deal(deck, 1)
       iex> hand
-      ["Ace of spades"]
+      ["Ace of Spades"]
   """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
